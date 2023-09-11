c7ac007070bf9178e3c994854513f2d256fc0261
pengtz <61131578+pengtz@users.noreply.github.com>
2023-09-11 21:06:19 +0800

    Update2 mcr_0.cpp
    Update version 2

--- mcr_0.cpp
+++ mcr_0.cpp
@@ -4,51 +4,75 @@
 
 
 bool isWin(char game[3][3]){
-	bool win = false;
-	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
-	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
-	// column
-	if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true;
-	if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
-	// diagonal
-	if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
-	return win;
+	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] != ' ')) return true;
+	if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] != ' ')) return true;
+	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] != ' ')) return true;
+// colum
+	if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] != ' ')) return true;
+	if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] != ' ')) return true;
+	if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] != ' ')) return true;
+// diagoal
+	if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] != ' ')) return true;
+	if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] != ' ')) return true;
+	return false;
 }
 
 int  main(){
 	int i, j;
 	char game[3][3] = {' '}; // Tic-tac-toe
-	char player1 = 'X';
-	char player2 = 'O';
-	bool turn = true; // false for player 1's turn, true for player 2's turn. Player 1 first.
-	std::cout << "X = Player 1" << std::endl << "O = Player 2" << std::endl;
-       for (int n=0; n<9; n++){
-			turn = !turn;  // use the not-operator to change true to false or false to true.
-		    if (turn == false)
-				std::cout << "Player 1: ";
-			else
-				std::cout << "Player 2: ";
-			std::cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
-			std::cin >> i >> j;
-			if (turn == false)
-			   game[i][j] = 'X';
-			else 
-			   game[i][j] = 'O';
-			if (isWin(game)){
-				std::cout << "Win!" << std::endl;
-				break; // need to terminate the problem
-			}
-		}
-	if (i==3) // all celles with i=0 to 2 have been inputted above but no winner yet
-	   std::cout << "Tie!" << std::endl;
+	//char playerl = 'X';
+	//char player2 = '0';
+	bool turn = true; // false forplayer l's turn, true forplayer 2's turn
 
-	// show the game to console
-	std::cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << std::endl;
-	std::cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << std::endl;
-	std::cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << std::endl;
+	bool legal = true;
+
+	int n = 0;
+	cout << "X = Player 1" << endl << "O = Player 2" << endl;
+	for(n = 0; n < 9; n++) {
+    		turn = !turn; // use the not-operator to change true to false or f
+    		if (turn == false){
+        		cout << "Player 1: ";
+    		}
+    		else{
+        		cout << "Player 2: ";
+    		}
+    		cout << "Which cellto mark? i:[0..2], j:[0..2]:";
+    		cin >> i >> j;
+
+		if('0' <= i && i <= '2' && '0' <= j && j <= '2'){
+        		if(game[i][j] == ' '){
+            			continue;
+        		}
+        		else{
+            			legal = false;
+            			break;
+        		}
+		}
+
+    		if  (turn == false){
+        		game[i][j] = 'X';
+    		}
+    		else{
+        		game[i][j] = '0';
+    		}
+    		if (isWin (game)) {
+        		cout << "Win!" << endl;
+        		break;// need to terminate the problem
+    		}
+	}
+	if(n == 9){
+    		cout << "Tie! " << endl;
+	} // allcelles with i=0 to 2 have been inputted above but no winner ye
+
+	if(legal == false){
+    		cout << "Illegal input" << endl;
+	}
+
+// show the game to console
+	cout << game[0][0] << " " << game[0][1] << " " << game[0][2]<< endl;
+	cout << game[1][0] << " " << game[1][1] << " " << game[1][2]<< endl;
+	cout << game[2][0] << " " << game[2][1] << " " << game[2][2]<< endl;
+
 	return 1;
 }
 
